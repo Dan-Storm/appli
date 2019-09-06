0ee5cfc6ccb43a2579f0ddefa74611478819961c
Dan Storm <stormdanp@gmail.com>
2019-09-06 09:24:33 -0700

    added Interview model

---
+++ Models/Interview.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Appli.Models
+{
+    public class Interview
+    {
+        public int Id { get; set; }
+
+        public int JobApplicationId { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string Address { get; set; }
+
+        public string Notes { get; set; }
+    }
+}
